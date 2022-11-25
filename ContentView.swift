5188e751f3ca0cf83006a2da6beb54b9bf1d3955
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-25 15:33:42 -0600

    Update ContentView, to be a real WelcomeView

--- ContentView.swift
+++ ContentView.swift
@@ -1,43 +1,35 @@
 //
 //  ContentView.swift
-//  iOS
+//  notification.ui
 //
-//  Created by Jayce Sagvold on 11/6/22.
+//  Created by Jayce Sagvold on 11/25/22.
 //
 
 import SwiftUI
 
+// ph = placeholder
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Username##11")
-                .foregroundColor(.blue)
-                .bold()
-                .font(.title2)
-            Text("User is not verified")
-                .bold()
+            // Have a image that is going to be used from SF Symbols
+            Image(systemName: "ph")
+            Text("Welcome to ___")
+                .font(.largeTitle)
+                .padding(.bottom, 25)
+            Image("WelcomeImage1") // This image will change depending on what the application is
+                .resizable()
+                .frame(width: 280, height: 300)
+                .cornerRadius(30)
+                .padding(.bottom, 25)
+            Text("A place that you have a welcome view.") // Change this
                 .font(.subheadline)
-                .foregroundColor(.accentColor)
-            Image(systemName: "flag.fill")
-                .imageScale(.medium)
-                .foregroundColor(.red)
-                .foregroundColor(.black)
-            
-            VStack {
-                Button("REPORT") { }
-                    .foregroundColor(.red)
-                    .buttonStyle(.bordered)
-                }
-            }
         }
     }
-
-
-struct 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
